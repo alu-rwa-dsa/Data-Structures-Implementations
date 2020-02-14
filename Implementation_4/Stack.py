d87513429280d86a500e5d510c34530cdeb9d6ff
ahmedmeshref <a.meshref@alustudent.com>
2020-02-14 15:35:42 +0200

    time and space complexity added

--- Implementation_4/Stack.py
+++ Implementation_4/Stack.py
@@ -7,11 +7,19 @@ class Stack(object):
         self.stack = []
 
     def show(self):
+        """
+        Time complexity: O(1)
+        Space complexity: O(1)
+        """
         return self.stack
 
     def push(self, item):
         """
         add a given element to the end of the stack
+        """
+        """
+        Time complexity: O(1)
+        Space complexity: O(1)
         """
         self.stack.append(item)
 
@@ -19,18 +27,30 @@ class Stack(object):
         """
         delete the last element of the stack
         """
+        """
+        Time complexity: O(1)
+        Space complexity: O(1)
+        """
         if self.size() > 0:
             self.stack.pop()
         else:
             raise IndexError
 
     def peek(self):
+        """
+        Time complexity: O(1)
+        Space complexity: O(1)
+        """
         try:
             return self.stack[-1]
         except IndexError:
             return None
 
     def size(self):
+        """
+        Time complexity: O(1)
+        Space complexity: O(1)
+        """
         return len(self.stack)
 
 
--- Implementation_4/queue.py
+++ Implementation_4/queue.py
@@ -10,12 +10,24 @@ class Queue:
         return self.queue
 
     def enqueue(self, val):
+        """
+        Time complexity: O(1)
+        Space complexity: O(1)
+        """
         self.queue.append(val)
 
     def dequeue(self):
+        """
+        Time complexity: O(1)
+        Space complexity: O(1)
+        """
         self.queue.pop(0)
 
     def peek(self):
+        """
+        Time complexity: O(1)
+        Space complexity: O(1)
+        """
         return self.queue[0]
 
 
